30c084abbc6734dd340a7336c439f8d89ffe6c03
Kenzo <kznw408@yahoo.com>
2012-10-31 20:16:30 -0200

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,7 @@ describe User do
 	it { should respond_to(:remember_token)}
 	it { should respond_to(:admin)}
 	it { should respond_to(:authenticate) }
+	it { should respond_to(:microposts)}
 
 	it { should be_valid}
 	it { should_not be_admin }
@@ -57,25 +58,25 @@ describe User do
 		it{ should_not be_valid }
 	end
 
-describe "when email format is invalid" do
-    it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
-        @user.should_not be_valid
-      end      
-    end
-  end
+	describe "when email format is invalid" do
+	    it "should be invalid" do
+	     	 addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+	                     foo@bar_baz.com foo@bar+baz.com]
+	      	addresses.each do |invalid_address|
+		        @user.email = invalid_address
+		        @user.should_not be_valid
+	     	end      
+	    end
+	end
 
-  describe "when email format is valid" do
-    it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
-        @user.should be_valid
-     	 end      
-   		end
+  	describe "when email format is valid" do
+   		it "should be valid" do
+	      	addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+	      	addresses.each do |valid_address|
+	       		@user.email = valid_address
+	        	@user.should be_valid
+	     	end      
+	   	end
 	end
 
 	describe "when email address is already taken" do
@@ -103,29 +104,62 @@ describe "when email format is invalid" do
 		it {should_not be_valid }
 	end
 
-	  describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
-  end
+	describe "with a password that's too short" do
+    	before { @user.password = @user.password_confirmation = "a" * 5 }
+    	it { should be_invalid }
+  	end
 
-  describe "return value of authenticate method" do
-    before { @user.save }
-    let(:found_user) { User.find_by_email(@user.email) }
-
-    describe "with valid password" do
-      it { should == found_user.authenticate(@user.password) }
-    end
-
-    describe "with invalid password" do
-      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
-      it { should_not == user_for_invalid_password }
-      specify { user_for_invalid_password.should be_false }
-    end
-
-    describe "remember token" do 
+  	describe "return value of authenticate method" do
     	before { @user.save }
-    	its(:remember_token) { should_not be_blank }
-    end
-  end
+    	let(:found_user) { User.find_by_email(@user.email) }
+
+    	describe "with valid password" do
+      		it { should == found_user.authenticate(@user.password) }
+   		end
+
+    	describe "with invalid password" do
+      		let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      		it { should_not == user_for_invalid_password }
+      		specify { user_for_invalid_password.should be_false }
+   		end
+
+   		describe "remember token" do 
+    		before { @user.save }
+    		its(:remember_token) { should_not be_blank }
+    	end
+  	end
+
+	describe "micropost associations" do 
+	  	before { @user.save }
+	  	let!(:older_micropost) do 
+	  		FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+	  	end
+	  	let!(:newer_micropost) do 
+	  		FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+	  	end
+
+	  	it "should have the right microposts in the right order" do 
+	  		@user.microposts.should == [newer_micropost, older_micropost]
+	  	end
+
+	  	it "should destroy associated microposts" do 
+	  		microposts = @user.microposts.dup
+	  		@user.destroy
+	  		microposts.should_not be_empty
+	  		microposts.each do |micropost|
+	  			Micropost.find_by_id(micropost.id).should be_nil
+	  		end
+	  	end
+
+    	describe "status" do
+      		let(:unfollowed_post) do
+        		FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      		end
+
+     	its(:feed) { should include(newer_micropost) }
+      	its(:feed) { should include(older_micropost) }
+      	its(:feed) { should_not include(unfollowed_post) }
+    	end
+  	end
 end
